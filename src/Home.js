ac3a4fd1a0072fcc11a139a96cf9cbdba8679f3b
muhammadthayyil <muhammad2015t@gmail.com>
2022-11-03 14:05:34 -0500

    changes

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+    </div>
+  );
+}
+ 
+export default Home;
